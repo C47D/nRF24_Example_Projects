63b6ad3dc55476c491e137853d93537caaab802c
C47D <carlos.santiago.diaz@gmail.com>
2017-09-03 20:19:08 -0500

    Update component functions

--- 01_Echo_Tx.cydsn/main.c
+++ 01_Echo_Tx.cydsn/main.c
@@ -1,7 +1,7 @@
 /*
  * 01_Echo_Tx
  * 
- * This project is the receiver side of the Echo project,
+ * This project is the transmitter side of the Echo project,
  * The nrf24 radio is configurd to transmit 1 byte and receive the same byte via
  * ACK with payload from the receiver radio.
  * The data sent is the char received with the UART component, when the receiver
@@ -11,6 +11,17 @@
 #include <stdbool.h>
 
 volatile bool irq_flag = false;
+volatile bool uart_flag = false;
+
+volatile unsigned char data;
+
+// This handler is executed when we get a byte via UART
+CY_ISR(UART_Handler)
+{
+    uart_flag = true;
+    data = UART_GetChar();
+    isr_UART_ClearPending();
+}
 
 // This handler is executed when we get an interrupt from
 // the IRQ pin of the radio
@@ -24,13 +35,13 @@ int main(void)
 {
     CyGlobalIntEnable;
     
-    const uint8_t RX_ADDR[5]= {0xBA, 0xAD, 0xC0, 0xFF, 0xEE};
+    const uint8_t TX_ADDR[5]= {0xBA, 0xAD, 0xC0, 0xFF, 0xEE};
     
     // Set the Handler for the IRQ interrupt
     isr_IRQ_StartEx(IRQ_Handler);
     
     nRF24_start();
-    nRF24_setRxPipe0Address(RX_ADDR, 5);
+    nRF24_setTxAddress(TX_ADDR, 5);
 
     while (1) {
         
@@ -40,15 +51,22 @@ int main(void)
             NrfIRQ flag = nRF24_getIRQFlag();
             nRF24_clearIRQFlag(flag);
             
-            unsigned char data;
+            // Get the data sent from the receiver
+            unsigned char received;
+            nRF24_getRxPayload(&received, 1);
             
-            // get the data from the transmitter
-            nRF24_getRxPayload(&data, 1);
-
-            // Send the data + ACK
-            nRF24_rxWritePayload(0, &data, 1);
+            // print the received char
+            UART_PutChar(received);
             
             irq_flag = false;
+        }
+        
+        if (true == uart_flag) {
+            
+            // Transmit the data from UART RX to nrf24 receiver
+            nRF24_PTX_Transmit(&data, 1);
+            
+            uart_flag = false;
         }
         
     }
